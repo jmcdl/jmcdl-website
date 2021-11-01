50744728529516de21377ec194f39332c0f79f18
James McDonell <james.mcdonell@outlook.com>
2021-11-01 14:50:32 -0500

    Get fonts working with emotion

---
+++ src/@types/emotion.d.ts
@@ -0,0 +1,9 @@
+import "@emotion/react";
+
+declare module "@emotion/react" {
+  export interface Theme {
+    breakpoints: { [key: string]: string };
+    colors: { [key: string]: string };
+    fonts: { [key: string]: string };
+  }
+}
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,4 @@
-import { Global, css } from "@emotion/react";
+import { Global, css, ThemeProvider, Theme } from "@emotion/react";
 
 export function App(): JSX.Element {
   const globalStyles = css`
@@ -10,13 +10,48 @@ export function App(): JSX.Element {
   `;
   console.log("globalStyles", globalStyles);
 
+  const theme: Theme = {
+    colors: {
+      black: "#111",
+      primary: "#9cb9dd",
+      primaryDark: "#6d88a9",
+      white: "#fff",
+    },
+    breakpoints: {
+      large: "1024px",
+      medium: "768px",
+      small: "640px",
+    },
+    fonts: {
+      bold: "Lato Bold, sans-serif",
+      light: "Lato Light, sans-serif",
+      regular: "Lato, sans-serif",
+    },
+  };
+
   return (
     <>
-      {/*<Global styles={globalStyles} />*/}
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <Global styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <h1
+          css={(theme) => ({
+            color: theme.colors.primary,
+            fontFamily: theme.fonts.regular,
+          })}
+        >
+          Hello
+        </h1>
+        <h1
+          css={(theme) => ({
+            color: theme.colors.primary,
+            fontFamily: theme.fonts.light,
+          })}
+        >
+          Hello
+        </h1>
+        <h1>Hello</h1>
+        <h1>Hello</h1>
+      </ThemeProvider>
     </>
   );
 }
